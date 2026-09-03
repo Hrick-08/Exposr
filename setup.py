03abd8747651ac7c78799a666036ecbbca730a5a
Hrick <hritabratadas8@gmail.com>
2026-09-03 14:55:51 +0530

    defining new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="exposr",
-    version="0.4.0",
+    version="0.5.0",
     packages=find_packages(),
     entry_points={"console_scripts": ["exposr=client.main:main"]},
 )
