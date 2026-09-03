0e30b47c0ab01aed737cc1dab81c165391c3ff54
Hrick <hritabratadas8@gmail.com>
2026-09-03 17:45:28 +0530

    updated version 0.5.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="exposr",
-    version="0.5.0",
+    version="0.5.1",
     description="Expose local TCP and UDP services to the public internet",
 
     long_description=long_description,
