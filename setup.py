ccad9e446c3e639eba8ad088465546d11d91e46d
Hrick <hritabratadas8@gmail.com>
2026-09-03 17:37:26 +0530

    setting up for pypi release

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from setuptools import find_packages, setup
 
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="exposr",
     version="0.5.0",
+    description="Expose local TCP and UDP services to the public internet",
+
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
     packages=find_packages(),
-    entry_points={"console_scripts": ["exposr=client.main:main"]},
+
+    entry_points={
+        "console_scripts": [
+            "exposr=client.main:main",
+        ],
+    },
+
+    python_requires=">=3.8",
 )
