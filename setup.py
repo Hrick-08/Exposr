6e0f8853bfc321fc168001cc5c6880827c773506
Hrick <hritabratadas8@gmail.com>
2026-09-04 21:46:56 +0530

    correcting version mismatch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="exposr",
-    version="0.5.3",
+    version="0.5.4",
     description="Expose local TCP and UDP services to the public internet",
 
     long_description=long_description,
